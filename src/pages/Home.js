68aac9e5f6a58a36146bf931d3a281152c0edad2
kwillard95 <kwillard95@gmail.com>
2020-02-24 17:15:04 -0800

    Sets up context api to enable the passing of data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRooms';
 import {Link} from 'react-router-dom';
 
 export default function Home() {
     return (
+        <>
         <Hero>
             <Banner title='Luxurious Rooms' subtitle="Delux rooms starting at $299">
                 <Link to='/rooms' className="btn-primary">
@@ -12,6 +15,9 @@ export default function Home() {
                 </Link>
             </Banner>
         </Hero>
+        <Services />
+        <FeaturedRooms />
+     </>
     )
 }
 
