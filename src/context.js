8616072cb50d128caa19b0545d3f5cada87b0a66
kwillard95 <kwillard95@gmail.com>
2020-02-26 14:39:57 -0800

    Implemented dropdown menu for filtering rooms

--- src/context.js
+++ src/context.js
@@ -12,7 +12,17 @@ class RoomProvider extends Component {
         rooms: [],
         sortedRooms: [],
         featuredRooms: [],
-        loading: true
+        loading: true,
+        type: 'all',
+        capacity: 1,
+        price: 0,
+        minPrice: 0,
+        maxPrice: 0,
+        minSize: 0,
+        maxSize: 0,
+        breakfast: false,
+        pets: false
+
     };
 
 
@@ -20,11 +30,16 @@ class RoomProvider extends Component {
         //this.getData
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
+        let maxPrice = Math.max(...rooms.map(item => item.price));
+        let maxSize = Math.max(...rooms.map(item => item.size));
         this.setState({
             rooms,
             featuredRooms,
             sortedRooms: rooms,
-            loading: false
+            loading: false,
+            price: maxPrice,
+            maxPrice,
+            maxSize
         })
     }
 
@@ -32,7 +47,7 @@ class RoomProvider extends Component {
         let tempItems = items.map((item) => {
             let id = item.sys.id;
             let images = item.fields.images.map(img => img.fields.file.url);
-            let room = { ...item.fields, images, id};
+            let room = { ...item.fields, images, id };
             return room;
         });
         return tempItems;
@@ -45,9 +60,26 @@ class RoomProvider extends Component {
         return room;
     }
 
+    handleChange = event => {
+        const target = event.target;
+        const value = event.type === 'checkbox' ? target.checked:
+        target.value;
+        const name = event.target.name;
+        this.setState({[name] : value}, this.filterRooms)
+       
+    }
+
+    filterRooms = () => {
+        console.log('hello')
+    }
+
     render() {
         return (
-            <RoomContext.Provider value={{...this.state, getRoom: this.getRoom}}>
+            <RoomContext.Provider value={{
+                ...this.state,
+                getRoom: this.getRoom,
+                handleChange: this.handleChange
+            }}>
                 {this.props.children}
             </RoomContext.Provider>
         )
@@ -59,11 +91,11 @@ const RoomConsumer = RoomContext.Consumer;
 //this is a higher order componenet, where a component will return another component
 
 export function withRoomConsumer(Component) {
-  return function ConsumerWrapper(props) {
-      return <RoomConsumer>
-          {value => <Component {...props} context={value}/>}
-      </RoomConsumer>
-  }
+    return function ConsumerWrapper(props) {
+        return <RoomConsumer>
+            {value => <Component {...props} context={value} />}
+        </RoomConsumer>
+    }
 }
 
 export { RoomProvider, RoomConsumer, RoomContext }
